47fa702384ae3970eb805ff96697b3e656b315da
AMG2-AMG2 <90682571+AMG2-AMG2@users.noreply.github.com>
2023-12-10 01:04:06 +0100

    Commit les 2
    Door goed afgemaakt met header footer een items alles gemaakt nu over naar les 3 afmaken.

---
+++ resources/views/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welkom bij uw website</title>
+</head>
+<body>
+    <div class="layout-container">
+        <header>
+            <div class="logo">
+                <img src="/resources/img/" alt="Logo Project">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#Ipad">Ipad</a></li>
+                    <li><a href="#Telefoon">Telefoon</a></li>
+                    <li><a href="#Laptop">Laptop</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div class="content">
+            @yield('content')
+        </div>
+
+        <footer>
+            <p>&copy; <?php echo date("Y"); ?>Website gemaakt!</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Welkom op Uw Website</h1>
+    <p>Dit is de inhoud van de homepagina.</p>
+@endsection
